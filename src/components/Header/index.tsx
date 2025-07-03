f81670c1dc9abe8a070e1dcb651c0fb81bd5fd62
Gabriel Paulini <paulini.gp@gmail.com>
2025-07-03 17:12:27 -0300

    [layout] header construction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,31 @@
-import { HeaderContainer } from './styles'
+import {
+  CartButton,
+  HeaderActionsContainer,
+  HeaderContainer,
+  LocationButton,
+} from './styles'
+import logo from '../../assets/logo.png'
+import { MapPin, ShoppingCartSimple } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <HeaderContainer>
-      <span>Logo</span>
-      <span>Options and settings...</span>
+      <NavLink to="/">
+        <img src={logo} alt="" />
+      </NavLink>
+      <HeaderActionsContainer>
+        <LocationButton title="Change Location">
+          <MapPin size={22} weight="fill" />
+          <span>Porto Alegre, RS</span>
+        </LocationButton>
+
+        <NavLink to="/checkout">
+          <CartButton addedItems={3}>
+            <ShoppingCartSimple size={22} weight="fill" />
+          </CartButton>
+        </NavLink>
+      </HeaderActionsContainer>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,3 +5,56 @@ export const HeaderContainer = styled.header`
   justify-content: space-between;
   align-items: center;
 `
+
+export const HeaderActionsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+`
+
+export const LocationButton = styled.button`
+  display: flex;
+  align-items: center;
+  column-gap: .25rem;
+  padding: .5rem;
+
+  background-color: ${props => props.theme['purple-light']};
+  color: ${props => props.theme['purple-dark']};
+
+  svg {
+    color: ${props => props.theme['purple']};
+  }
+`
+
+type CartProps = {
+  addedItems?: number
+}
+
+export const CartButton = styled.button<CartProps>`
+  padding: 0.5rem;
+  background-color: ${props => props.theme['yellow-light']};
+  color: ${props => props.theme['yellow-dark']};
+  position: relative;
+
+  &::before {
+    content: '${props => props.addedItems}';
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1.25rem;
+    height: 1.25rem;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(40%, -40%);
+
+
+    background-color: ${props => props.theme['yellow-dark']};
+    color: white;
+    border-radius: 50%;
+
+    ${props => props.theme.fontStyles['text-s']}
+  }
+`
